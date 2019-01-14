06192fe115468a3484b2a093ebfacda203f1ea47
Luca Orlandini <luca.orlandini@docomodigital.com>
2019-01-14 17:22:10 +0100

    base source

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class HelloMessage extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>Hello {this.props.name}</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+let App = document.getElementById("root");
+
+ReactDOM.render(<HelloMessage name="Luca" />, App);
